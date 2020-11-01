bf38af505d9b1a93c335e50faf534c61e30287c0
suda_r <38130790+sudarshanstark@users.noreply.github.com>
2020-11-01 19:58:34 +0530

    Add files via upload

---
+++ interfaces.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Abser interface {
+	Abs() float64
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	var a Abser
+	v := Vertex{3, 4}
+	f := MyFloat(-math.Sqrt2)
+	a = &v // a *Vertex implements Abser
+	a = f  // MyFloat implements Abser
+	fmt.Println(a.Abs())
+}
